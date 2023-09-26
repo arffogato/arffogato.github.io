eb5928109da30e8e5cac06976deda948a1836f57
Ed Salinas <edsalinas7b@yahoo.com>
2023-09-27 06:49:18 +0800

    Add reviews

--- assets/js/main.js
+++ assets/js/main.js
@@ -404,6 +404,24 @@
   /**
    * Testimonials slider
    */
+   new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Events slider
+   */
    new Swiper('.events-slider', {
     speed: 600,
     loop: true,
